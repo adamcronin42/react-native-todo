0e092fcbf6f24711b07cb9ab5c74ae0980e31857
Adam <adamcronin42@gmail.com>
2019-01-23 15:48:38 -0600

    Adding redux-persist to persist data locally

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ import {name as appName} from './app.json';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { reducer } from './redux/reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = createStore(reducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 const AppWithStore = () => (
   <Provider store={store}>
-    <App /> 
+    <PersistGate loading={null} persistor={persistor}>
+      <App /> 
+    </PersistGate>
   </Provider>
 )
 
